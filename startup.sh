9d0a827fe48a9ea77d0c39f94ad67bb5d7c2c50e
jtmk4752 <jtmk4752@yahoo.co.jp>
2021-11-12 15:05:50 +0900

    adding code on the crontab file (related /sys/module/pcie_aspm/parameters/policy)

--- startup.sh
+++ startup.sh
@@ -7,3 +7,6 @@ sleep 5
 sudo nvpmodel -m 0
 sleep 5
 sudo jetson_clocks
+sleep 5
+#https://forums.developer.nvidia.com/t/pcie-bus-error-severity-corrected-on-jetson-nano/155780/3
+echo "performance" | sudo tee /sys/module/pcie_aspm/parameters/policy
