9ba1979b198fd7af6512c7ea000b103784c53fd9
jtmk4752 <jtmk4752@yahoo.co.jp>
2021-11-17 09:20:08 +0900

    minute change (added comment)

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #do with sudo in docker container
-#We need dlib installation manually since cuBLAS is mounted after "docker start"(and can't use CUDA on dlib).
+#(sudo bash ./setup.sh)
+#We need dlib installation manually since cuBLAS is mounted after "docker start" (and can't use CUDA on dlib).
 #So, we can't install dlib on the Docker file.
 #refer
 #https://forums.developer.nvidia.com/t/nvidia-l4t-base-missing-cublas-v2-h/174582/5
--- startup.sh
+++ startup.sh
@@ -2,11 +2,16 @@
 
 #http://www.neko.ne.jp/~freewing/raspberry_pi/nvidia_jetson_nano_cron_crontab_autoexec_script/
 
-#jetson nano用。crontabに追加。setup.shでセットアップするのも良い
+#FOR JETSON NANO 
+#Add "crontab -e"
+
 sleep 5
 sudo nvpmodel -m 0
 sleep 5
 sudo jetson_clocks
 sleep 5
+
+#for continuous running
+#refer
 #https://forums.developer.nvidia.com/t/pcie-bus-error-severity-corrected-on-jetson-nano/155780/3
 echo "performance" | sudo tee /sys/module/pcie_aspm/parameters/policy
